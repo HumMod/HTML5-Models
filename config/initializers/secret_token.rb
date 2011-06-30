7e1e8031fad43c2b5ef6ae9ac3561b60eaba5794
Leland Husband <leland.husband@gmail.com>
2011-06-30 15:23:45 -0500

    Adding Rails basic structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blank::Application.config.secret_token = 'af10e0a761dd98c83d2c1ec0509a0ad3a6b05b78f87e06e84e5e72a2c7c33397f3a91ea079969afb5d7e96638702c07600cdb8cf9635d0e86cd0020e3ca5b44e'
